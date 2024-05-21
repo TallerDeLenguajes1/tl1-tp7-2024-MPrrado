9b5671f1a893a19232d77a4c2c982e73348df808
Prado Matias Santiago <matias14prado@gmail.com>
2024-05-21 13:57:16 -0300

    Se completo ejercicio 1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using System.Collections.Concurrent;
+using EspacioCalculadora; //necesitamos esto para traer el espacio de nombres (el entorno) de nuestra clase para poder acceder
+Calculadora instanciaCalculadora = new Calculadora();
+
+static bool operacionCalculadora(Calculadora instanciaCalculadora, bool salir, int opcion)
+{
+    double termino;
+    switch (opcion)
+    {
+        case 1:
+            System.Console.WriteLine("INGRESE EL TERMINO A SUMARSE: ");
+            if(double.TryParse(Console.ReadLine(), out termino)){
+                instanciaCalculadora.Sumar(termino);
+            }else{
+                System.Console.WriteLine("ERROR EL INGRESE UN NUMERO VALIDO");
+            }
+            break;
+
+        case 2:
+            System.Console.WriteLine("INGRESE EL TERMINO A RESTARSE: ");
+            if(double.TryParse(Console.ReadLine(), out termino)){
+                instanciaCalculadora.Restar(termino);
+            }else{
+                System.Console.WriteLine("ERROR EL INGRESE UN NUMERO VALIDO");
+            }
+            break;
+
+        case 3:
+            System.Console.WriteLine("INGRESE EL TERMINO A SUMARSE: ");
+            if(double.TryParse(Console.ReadLine(), out  termino)){
+                instanciaCalculadora.Multiplicar(termino);
+            }else{
+                System.Console.WriteLine("ERROR EL INGRESE UN NUMERO VALIDO");
+            }
+            break;
+
+        case 4:
+            System.Console.WriteLine("INGRESE EL TERMINO A SUMARSE: ");
+            if(double.TryParse(Console.ReadLine(), out  termino)){
+                instanciaCalculadora.Dividir(termino);
+            }else{
+                System.Console.WriteLine("ERROR EL INGRESE UN NUMERO VALIDO");
+            }
+            break;
+
+        case 5:
+            System.Console.WriteLine($"EL RESULTADO FINAL ES: {instanciaCalculadora.Resultado}");
+            break;
+
+        case 6:
+            instanciaCalculadora.limpiar();
+            break;
+        
+        default:
+            Console.WriteLine("saliendo....");
+            salir = false;
+            break;
+    }
+    return salir;
+}
+
+bool salir = true;
+while(salir)
+{
+    Console.WriteLine("--------MENU CALCUADORA V2.0.0-------");
+    Console.WriteLine("1- SUMAR");
+    Console.WriteLine("2- RESTAR");
+    Console.WriteLine("3- MULTIPLICAR");
+    Console.WriteLine("4- DIVIDIR");
+    Console.WriteLine("5- MOSTRAR RESULTADO");
+    Console.WriteLine("6- LIMPIAR");
+    Console.WriteLine("0- SALIR");
+    Console.WriteLine("Ingrese una opcion: ");
+    if (int.TryParse(Console.ReadLine(), out int opcion))
+    {
+        if (opcion > 5 || opcion < 0)
+        {
+            Console.WriteLine("ERROR LA OPCION INGRESADA NO ES VALIDA");
+        }
+        else
+        {
+            salir = operacionCalculadora(instanciaCalculadora,salir, opcion);
+        }
+    }
+}
---
+++ ejercicio1/calculadora.cs
@@ -0,0 +1,33 @@
+namespace EspacioCalculadora; //analogia con compañeros de clase y otra clase
+public class Calculadora
+{
+    private double dato;
+
+    private double Dato{ //esto no es necesario porque yo PUEDO acceder a las variables que son privadas mientras este dentro de la clase
+        get; set;
+    }
+    
+
+    public double Resultado{ //aqui solo creo la propiedad de Resultado para poder acceder a traver de ella a lo que contiene el resultado que lo alamaceno en dato
+        get => Dato;
+    }
+    public void Sumar(double termino){
+        Dato = Dato + termino;
+    }
+    public void Restar(double termino){
+        Dato = Dato - termino;
+    }
+    public void Multiplicar(double termino){
+        Dato = Dato * termino;
+    }
+    public void Dividir(double termino){
+        Dato = Dato + termino;
+    }
+    public void limpiar(){
+        Dato = 0;
+    }
+
+    
+
+
+}
