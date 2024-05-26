759fc2899d13658bcc50ff9cbb85858c010d1199
Prado Matias Santiago <matias14prado@gmail.com>
2024-05-25 22:38:52 -0300

    Fin ejercicio 2. Fin del tp7

--- ejercicio2/Program.cs
+++ ejercicio2/Program.cs
@@ -1,4 +1,5 @@
-﻿using Empleados;
+﻿using System.Runtime.CompilerServices;
+using Empleados;
 
 Empleado[] trabajadores = new Empleado[3];  // asi es como declaramos 3 instancias (QUE NO ESTAN INSTANCIADAS) de nuestra clase Empleados
 string[] nombres = {"Juan", "Pedro", "Matias"};
@@ -11,16 +12,56 @@ Random anioNacimiento = new Random();
 Random anioIngreso = new Random();
 Random mes = new Random();
 Random dia = new Random();
-
+double totalGastosSueldos=0;
 for(int i = 0; i < trabajadores.Length; i++)
 {
     trabajadores[i] = new Empleado();
     trabajadores[i].Cargo = Cargos.Ingeniero; //seleccionamos el tipo Cargos como muestra esta linea
     trabajadores[i].FechaIngreso = new DateTime(anioIngreso.Next(2000,2024), mes.Next(1,12), dia.Next(1,31)); 
     trabajadores[i].FechaNacimiento = new DateTime(anioNacimiento.Next(1970,1985), mes.Next(1,12), dia.Next(1,31)); 
-    trabajadores[i].Nombre = nombres[indice.Next(2)];
-    trabajadores[i].Apellido = apellidos[indice.Next(2)];
-    trabajadores[i].SueldoBasico = sueldoBasico.Next(15000000,50000000)/100; 
-    trabajadores[i].EstadoCivil = estadoCivil[indiceEstadoCivil.Next(1)];
-    
+    trabajadores[i].Nombre = nombres[indice.Next(0,2)];
+    trabajadores[i].Apellido = apellidos[indice.Next(0,2)];
+    trabajadores[i].SueldoBasico = Math.Round(sueldoBasico.NextDouble() * (500000-150000) + 150000,2); 
+    trabajadores[i].EstadoCivil = estadoCivil[indiceEstadoCivil.Next(0,1)];
+    totalGastosSueldos = totalGastosSueldos + trabajadores[i].CalcularSalario();
+
+    //tambien puedo hacer de la forma simplificada
+    // trabajadores[i] = new Empleado(){
+
+    //     Cargo = Cargos.Ingeniero; //seleccionamos el tipo Cargos como muestra esta linea
+    //     FechaIngreso = new DateTime(anioIngreso.Next(2000,2024), mes.Next(1,12), dia.Next(1,31)); 
+    //     FechaNacimiento = new DateTime(anioNacimiento.Next(1970,1985), mes.Next(1,12), dia.Next(1,31)); 
+    //     Nombre = nombres[indice.Next(0,2)];
+    //     Apellido = apellidos[indice.Next(0,2)];
+    //     SueldoBasico = Math.Round(sueldoBasico.NextDouble() * (500000-150000) + 150000,2); 
+    //     EstadoCivil = estadoCivil[indiceEstadoCivil.Next(0,1)];
+    // }
 }
+
+//mostramos los campos de cada empleado
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("----------DATOS EMPLEADOS----------");
+for(int i = 0; i < trabajadores.Length; i++)
+{
+    trabajadores[i].mostrarEmpleado(i);
+}
+
+//mostramos gastos totales en caracter de salarios
+Console.ForegroundColor = ConsoleColor.Blue;
+System.Console.WriteLine($"-----Gastos totales en caracter de salarios: ${totalGastosSueldos}");
+
+//buscamos el empleado mas proximo de la jubilacion
+int indiceProxJubilo = 0;
+int checkVar =0 ;
+for (int i=0; i < trabajadores.Length; i++)
+{
+    if (trabajadores[i].EdadEmpleado() > checkVar)
+    {
+        checkVar = trabajadores[i].EdadEmpleado();
+        indiceProxJubilo = i;
+    }
+}
+Console.ForegroundColor = ConsoleColor.Green;
+System.Console.WriteLine("--------------EMPLEADO PROXIMO A JUBILARSE--------------");
+trabajadores[indiceProxJubilo].mostrarEmpleado(indiceProxJubilo);
+
--- ejercicio2/empleado.cs
+++ ejercicio2/empleado.cs
@@ -1,4 +1,7 @@
 
+using System.Diagnostics;
+using System.Security.Cryptography;
+
 namespace Empleados{
     enum Cargos
     {
@@ -11,6 +14,11 @@ namespace Empleados{
 
     class Empleado
     {
+        const double EXTRA_MAX_POR_ANTIGUEDAD = 0.25;
+        const double MULTIPLICADOR_EXTRA_POR_ANTIGUEDAD = 0.01;
+        const double EXTRA_POR_ESTAR_CASADO = 150000;
+        const double MULTIPLICADOR_EXTRA_POR_CARGO = 1.5;
+        const int ANTIGUEDAD_MAX = 20;
         const int ANIOS_PARA_JUBILACION = 65;
         private string nombre;
         private string apellido;
@@ -28,7 +36,7 @@ namespace Empleados{
         internal Cargos Cargo { get => cargo; set => cargo = value; }
         public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
 
-        public int Antiguedad(DateTime FechaIngreso)
+        public int Antiguedad()
         {
             int Antiguedad = 0;
             DateTime FechaActual = DateTime.Now;
@@ -36,7 +44,7 @@ namespace Empleados{
             return Antiguedad;
         }
 
-        public int EdadEmpleado(DateTime FechaNacimiento)
+        public int EdadEmpleado()
         {
             int edad;
             DateTime FechaActual = DateTime.Now;
@@ -44,17 +52,59 @@ namespace Empleados{
             return edad;
         }
 
-        public int AniosParaRetiro(DateTime FechaIngreso)
+        public int AniosParaRetiro()
         {
             int aniosParaRetiro;
-            aniosParaRetiro = ANIOS_PARA_JUBILACION - Antiguedad(FechaIngreso);
+            aniosParaRetiro = ANIOS_PARA_JUBILACION - EdadEmpleado();
             return aniosParaRetiro;
         }
 
-        public double CalcularSalario(double sueldoBasico)
+        public double CalcularSalario()
         {
             double salario=0;
-            return salario;
+            double adicional;
+            if (Antiguedad()>ANTIGUEDAD_MAX)
+            {
+                adicional = sueldoBasico* EXTRA_MAX_POR_ANTIGUEDAD;
+            }else
+            {
+                adicional =sueldoBasico * Antiguedad()* MULTIPLICADOR_EXTRA_POR_ANTIGUEDAD;
+            }
+            if(estadoCivil == 'c')
+            {
+                adicional = adicional + EXTRA_POR_ESTAR_CASADO;
+            }
+
+            if((cargo.ToString() == Cargos.Ingeniero.ToString()) || (cargo.ToString() == Cargos.Especialista.ToString()))
+            {
+                adicional = adicional * MULTIPLICADOR_EXTRA_POR_CARGO;
+            }
+            salario = sueldoBasico + adicional;
+            return Math.Round(salario, 2);
+        }
+
+        public void mostrarEmpleado(int indiceEmpleado)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Los datos del empleado {indiceEmpleado + 1} son: ");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Apellido: {Apellido}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Edad: {EdadEmpleado()}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Fecha de Nacimientos: {FechaNacimiento.ToShortDateString()}");
+            Console.WriteLine($"Estado Civil (c: casado, s: soltero): {EstadoCivil}");
+            Console.WriteLine($"Fecha Ingreso: {FechaIngreso.ToShortDateString()}");
+            Console.WriteLine($"Cargo: {Cargo}");
+            Console.WriteLine($"Sueldo Basico: ${SueldoBasico}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Antiguedad: {Antiguedad()} anios");
+            Console.WriteLine($"Salario: ${CalcularSalario()}");
+            Console.WriteLine($"Anios para el retiro: {AniosParaRetiro()}");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("------------------------------");
         }
 
     }
